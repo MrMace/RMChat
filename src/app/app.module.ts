e3f563c0298dd0a01ca33e72cdb14379313b8964
mattt <mace.matt@umail.iu.edu>
2017-12-04 22:20:45 -0500

    RM/Chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,39 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+import {AngularFirestoreModule} from "angularfire2/firestore";
+
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { AuthorizeProvider } from '../providers/authorize/authorize';
+import { FIREBASE_CONFIG } from './firebaseConfig';
+
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+      AngularFireModule.initializeApp(FIREBASE_CONFIG),
+      AngularFirestoreModule.enablePersistence(),
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthorizeProvider,
+      AngularFireAuth
   ]
 })
 export class AppModule {}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import {userCreds} from "../../models/interfaces/userCredentials";
+
+import {AuthorizeProvider} from '../../providers/authorize/authorize'
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  credentials = {} as userCreds;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private authorizeService: AuthorizeProvider) {
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+    signin() {
+        this.authorizeService.login(this.credentials).then((res: any) => {
+            if (!res.code)
+                this.navCtrl.setRoot('TabsPage');
+            else
+                alert(res);
+        })
+    }
+
+    signup() {
+        this.navCtrl.push('SignupPage');
+    }
+
+    resetPassword() {
+        this.navCtrl.push('PasswordresetPage');
+    }
+}
